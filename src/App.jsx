8a0b06022a80f083931cab1fe91e2e315d81a5b6
JabuPavan <jabupavanpatel@gmail.com>
2026-06-16 01:24:22 +0530

    Add Vercel Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Analytics } from "@vercel/analytics/react";
 import Portfolio from './Portfolio';
 import DashboardLayout from './pages/admin/DashboardLayout';
 import './index.css';
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Portfolio />} />
-        <Route path="/admin/*" element={<DashboardLayout />} />
-      </Routes>
-    </Router>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Portfolio />} />
+          <Route path="/admin/*" element={<DashboardLayout />} />
+        </Routes>
+      </Router>
+      <Analytics />
+    </>
   );
 };
 
